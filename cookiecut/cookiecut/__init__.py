c2eb16173b5ae1dcf4933d323a459a104fc5d062
Kaytheman99 <henrik.edlund123@gmail.com>
2024-02-27 00:49:23 +0100

    Add feature to cookiecut/__init__.py

--- cookiecut/cookiecut/__init__.py
+++ cookiecut/cookiecut/__init__.py
@@ -1,5 +1,5 @@
-"""Top-level package for cookiecut-temp"""
+"""Top-level package for cookiecut"""
 
-__author__ = """Henrik Edlund"""
+__author__ = """Kaytheman99"""
 __email__ = 'henrik.edlund123@gmail.com'
 __version__ = '0.1.0'
